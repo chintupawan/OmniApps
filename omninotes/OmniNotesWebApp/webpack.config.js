1018558c44d13064a51ce19757bb248f76995060
chintupawan <chintupawan@gmail.com>
2018-03-20 23:31:40 +1300

    Added basic styles

--- omninotes/OmniNotesWebApp/webpack.config.js
+++ omninotes/OmniNotesWebApp/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './index.tsx',
+    entry: './src/index.tsx',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'wwwroot/js')
